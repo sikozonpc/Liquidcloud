a1eb49897c0c5133a27aa81cc16a4e6a2e92377a
sikozonpc <sikozonpc@gmail.com>
2019-03-15 14:13:02 +0000

    responsive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 window.onload = ()  =>  console.log("Hello user! If you are not a developer you shouln't be here.");
+
+// Remove animation classes from AOS library at mobile breakpoint
+let currentScreenWidth = window.innerWidth;
+console.log(currentScreenWidth);
+if(currentScreenWidth <= 500) console.log(currentScreenWidth);
