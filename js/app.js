3b47bc61cfcc303ec17bb8c3ab41420385ca4045
sikozonpc <sikozonpc@gmail.com>
2019-03-15 18:38:25 +0000

    footer done but needs responsiveness

--- js/app.js
+++ js/app.js
@@ -3,4 +3,13 @@ window.onload = ()  =>  console.log("Hello user! If you are not a developer you 
 // Remove animation classes from AOS library at mobile breakpoint
 let currentScreenWidth = window.innerWidth;
 console.log(currentScreenWidth);
-if(currentScreenWidth <= 500) console.log(currentScreenWidth);
+if(currentScreenWidth <= 500) {
+    let fade = document.querySelectorAll("#fade");
+ 
+    console.log(fade)
+    for(let i=0; i < fade.length; i++){
+        fade[i].attributes["data-aos"].value = "HIDDEN";
+    }
+    
+    console.log(fade)
+}
