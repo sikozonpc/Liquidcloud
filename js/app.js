336232ac7e816f1d7dc096951cec24e5b6a3cfd8
sikozonpc <sikozonpc@gmail.com>
2019-03-17 16:08:37 +0000

    almost compleated

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 window.onload = ()  =>  console.log("Hello user! If you are not a developer you shouln't be here.");
 
+
+
 // Remove animation classes from AOS library at mobile breakpoint
-let currentScreenWidth = window.innerWidth;
-console.log(currentScreenWidth);
-if(currentScreenWidth <= 500) {
-    let fade = document.querySelectorAll("#fade");
- 
-    console.log(fade)
-    for(let i=0; i < fade.length; i++){
-        fade[i].attributes["data-aos"].value = "HIDDEN";
+window.onresize = function responsive(){
+    let currentScreenWidth = window.innerWidth;
+
+    if(currentScreenWidth <= 500) {
+        let fade = document.querySelectorAll("#fade");
+     
+        for(let i=0; i < fade.length; i++){
+            fade[i].attributes["data-aos"].value = "HIDDEN";
+        }
     }
-    
-    console.log(fade)
 }
