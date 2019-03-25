84af3707cec98a2a8434935f52c48222256897b6
sikozonpc <sikozonpc@gmail.com>
2019-03-25 22:40:50 +0000

    stable

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 window.onload = ()  =>  console.log("Hello user! If you are not a developer you shouln't be here.");
 
 
-
 // Remove animation classes from AOS library at mobile breakpoint
-window.onresize = function responsive(){
+window.onresize = removeFadeEffects();
+window.onload =   removeFadeEffects();
+
+const creditsPopup = document.querySelector("#creditsPopup");
+const hamButton = document.querySelector(".fa-bars");
+const mobileNav = document.querySelector(".mobile");
+
+
+function removeFadeEffects() {
     let currentScreenWidth = window.innerWidth;
 
     if(currentScreenWidth <= 500) {
@@ -13,4 +20,47 @@ window.onresize = function responsive(){
             fade[i].attributes["data-aos"].value = "HIDDEN";
         }
     }
+}
+
+
+// Mobile Navbar 
+function openMobile() {
+    mobileNav.classList.add("open");
+}
+function closeMobile() {
+    mobileNav.classList.remove("open");
+}
+
+hamButton.addEventListener("click", openMobile);
+mobileNav.addEventListener("click", closeMobile);  
+
+
+
+// When the user scrolls down x from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    document.getElementById("topButton").style.transform = "translateY(0)";
+  } else {
+    document.getElementById("topButton").style.transform = "translateY(100vh)";
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function toTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+
+
+// Credits popup code 
+function showCreditsPopup() {
+  creditsPopup.style.transform = "translateY(0)";
+
+}
+function hideCreditsPopup() {
+  creditsPopup.style.transform = "translateY(100vh)";
 }
